2f0f17fc6a294ab04cd4c51b21f75d222d9c528e
Mathias Oxholm <mo.micheelsen@gmail.com>
2017-10-14 01:57:28 +0200

    Added form.php

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <link href="ball.css" rel="stylesheet" type="text/css"/>
+    <!--<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">-->
+  </head>
+  <body>
+    <div class="wrapper">
+      <h1>JimBot 2.0<sup>&reg;</sup></h1>
+      <form class="form" action="8ball.php" method="post">
+          <input type="text" name="q" autofocus placeholder="Ask me something..." required>
+          <input class="btn" type="submit" name="submit" value="Go!">
+      </form>
+    </div>
+  </body>
+</html>
